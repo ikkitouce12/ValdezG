2f9013b2bb82ff4b7afa037ca60c21197c78d1e5
ikkitouce12 <valdezgerardo734@gmail.com>
2022-09-07 13:51:03 -0500

    Segunda Version

--- suma.cpp
+++ suma.cpp
@@ -3,9 +3,12 @@ using namespace std;
 int main ()
 {
 	float A,B,C;
-	cin>>A>>B;
+	cout<<"Ingrese 2 numeros A B :";cin>>A>>B;
+
 	C=A+B;
-	cout<<C;
+	cout<<"El resultado es :";
+        cout<<C;
+
 
 	return(0);
 
